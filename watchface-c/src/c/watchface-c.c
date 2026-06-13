021a7a56ca6243e2e2e78c0d0929f7248ba69648
Jiatao Liang <jtliang24@gmail.com>
2026-06-13 01:38:10 -0400

    feat(watchface-c): implement time and date layers with tick handler

--- watchface-c/src/c/watchface-c.c
+++ watchface-c/src/c/watchface-c.c
@@ -1,12 +1,52 @@
 # include <pebble.h>
 static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
+
+static void update_time() {
+    time_t temp = time(NULL);
+    struct tm *tick_time = localtime(&temp);
+
+    static char s_time_buffer[8];
+    strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+
+    static char s_date_buffer[16];
+    strftime(s_date_buffer, sizeof(s_date_buffer), "%a, %b %d", tick_time);
+
+    text_layer_set_text(s_time_layer, s_time_buffer);
+    text_layer_set_text(s_date_layer, s_date_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    update_time();
+}
 
 static void main_window_load(Window *window) {
+    // Get information about the window
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
+
+    // Create time TextLayer
+    s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+    text_layer_set_background_color(s_time_layer, GColorClear);
+    text_layer_set_text_color(s_time_layer, GColorWhite);
+    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+    s_date_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(110, 104), bounds.size.w, 30));
+    text_layer_set_background_color(s_date_layer, GColorClear);
+    text_layer_set_text_color(s_date_layer, GColorWhite);
+    text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+    text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+    layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+    layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 
 }
 
 static void main_window_unload(Window *window) {
-
+    text_layer_destroy(s_time_layer);
+    text_layer_destroy(s_date_layer);
 }
 
 static void init() {
@@ -25,12 +65,18 @@ static void init() {
   // show window on watch with animated=true
   window_stack_push(s_main_window, true);
 
+  // Display time from the start
+  update_time();
+
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
 }
 
 static void deinit() {
+  tick_timer_service_unsubscribe();
+
   // Destory window
   window_destroy(s_main_window);
-
 }
 
 int main(void) {
